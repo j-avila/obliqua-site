a10b46b729defc65f21a2425f1c7092effadec67
j-avila <jose.leon.avila@gmail.com>
2023-08-21 02:00:33 -0400

    added deploy &  settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config"
+import react from "@astrojs/react"
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://j-avila.github.io",
+  base: "/obliqua-site",
   integrations: [react(), tailwind()],
   experimental: {
-    assets: true
-  }
-});
+    assets: true,
+  },
+})
